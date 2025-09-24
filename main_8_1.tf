0b22e99eda2e6df4baab59c5794acd00e5c7dbe4
kalawads <shilpaworld133@gmail.com>
2025-09-24 12:18:01 +0530

    name tag added

--- main_8_1.tf
+++ main_8_1.tf
@@ -1,7 +1,7 @@
 resource "aws_vpc" "shilpavpc" {
-  cidr_block = "10.33.0.0/16"
+  cidr_block = "10.133.0.0/16"
   tags = {
-    name = "shilpa-gitvpc"
+    Name = "shilpa-gitvpc"
     env = "env from local system"
   }
 }
